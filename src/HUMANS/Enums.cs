c9d0ddc4975bb5ed3488a13dc37a273e364f235e
Falki <72734856+Falki-git@users.noreply.github.com>
2023-08-11 21:14:13 +0200

    Some skin definitons added #2

---
+++ src/HUMANS/Enums.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Humans
+{
+    /// <summary>
+    /// Color Palette from https://huebliss.com/skin-color-code/
+    /// </summary>
+    public enum SkinType
+    {
+        Real,
+        Fair,
+        Dark,
+        Caucasian,
+        Indian,
+        BeautifulTouch,
+        Ebony,
+        NormalAndTan,
+        Natural,
+        Asian,
+        Black
+    }
+
+    public enum Culture
+    {
+        American,
+        European,
+        Asian
+    }
+
+    public enum Nation
+    {
+        USA,
+        Canada,
+        Germany,
+        France,
+        // ...
+    }
+
+    // TODO Culture -> has % of skin types
+    // TODO Nationality -> has culture assigned. Skin type % per nationality, defaults to assigned culture if not set
+    // TODO loading screens
+    // TODO first names and surnames for each nationality
+    // TODO hair color, facial hair color
+
+}
