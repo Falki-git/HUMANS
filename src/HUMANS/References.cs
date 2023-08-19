7cd2a57ed49970f225cbab3f99be1b177fa4f7f7
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-08-19 20:08:39 +0200

    Add references file to document presets origins

---
+++ src/HUMANS/References.cs
@@ -0,0 +1,35 @@
+﻿
+/*
+
+Source material
+
+# Nationality and Culture
+    ## https://github.com/KSP-RO/KerbalRenamer
+        - Nationality per Culture weights
+        - First and last names per Nation
+        - TODO: copy licence
+
+# Skin colors
+    ## https://huebliss.com/skin-color-code/
+        - Skin types: Real, Fair, Dark, Caucasian, Indian
+
+    ## https://www.schemecolor.com/
+        ### https://www.schemecolor.com/beautiful-touch.php
+            - Skin type: BeautifulTouch
+
+        ### https://www.schemecolor.com/ebony-skin.php
+            - Skin type: Ebony
+
+        ### https://www.schemecolor.com/normal-and-tan-skin-colors.php
+            - Skin type: Natural
+
+        ### https://www.schemecolor.com/asian-skin.php
+            - Skin type: Asian
+
+        ### https://www.schemecolor.com/black-guy-skin-colors.php
+            - Skin type: Black
+
+    ## original Kerbal skins - in-game assets
+
+
+*/
