cee4f6b2900a9bf0ec38d4e6626a85d833957104
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-08-21 00:24:57 +0200

    Add KerbalUtility, fetches kerbal attributes

---
+++ src/HUMANS/Utilities/KerbalUtility.cs
@@ -0,0 +1,36 @@
+﻿using KSP.Game;
+using KSP.Sim;
+using KSP.Sim.impl;
+using UnityEngine;
+
+namespace Humans.Utilities
+{
+    public static class KerbalUtility
+    {
+        private static KerbalInfo _kerbalInfo;
+        
+        public static void SetKerbal(KerbalInfo kerbalInfo) => _kerbalInfo = kerbalInfo;
+
+        public static IGGuid IGGuid { get => _kerbalInfo.Id; }
+        public static KerbalInfo KerbalInfo { get => _kerbalInfo; }
+        public static string NameKey { get => _kerbalInfo.NameKey; }
+        public static string FirstName { get => _kerbalInfo.Attributes.FirstName; }
+        public static string Surname { get => _kerbalInfo.Attributes.Surname; }
+        public static KerbalType HumanType { get => (KerbalType)_kerbalInfo.Attributes.GetAttribute("TYPE"); }
+        public static string HairStyle { get => (string)_kerbalInfo.Attributes.GetAttribute("HAIRSTYLE"); }
+        public static string Helmet { get => (string)_kerbalInfo.Attributes.GetAttribute("HELMET"); }
+        public static Color HairColor { get => (Color)_kerbalInfo.Attributes.GetAttribute("HAIRCOLOR"); }
+        public static string Eyes { get => (string)_kerbalInfo.Attributes.GetAttribute("EYES"); }
+        public static Single EyeHeight { get => (Single)_kerbalInfo.Attributes.GetAttribute("EYEHEIGHT"); }
+        public static Single EyeSymmetry { get => (Single)_kerbalInfo.Attributes.GetAttribute("EYESYMMETRY"); }
+        public static Color SkinColor { get => (Color)_kerbalInfo.Attributes.GetAttribute("SKINCOLOR"); }
+
+        
+        
+        
+        
+        
+        public static string Head { get => (string)_kerbalInfo.Attributes.GetAttribute("HEAD"); }
+
+    }
+}
