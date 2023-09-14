0ec34a428495943e971425bc8e72cc9015ef25ba
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-09-14 12:54:27 +0200

    Add tabs

--- src/HUMANS/Presets/CulturePresets.cs
+++ src/HUMANS/Presets/CulturePresets.cs
@@ -34,6 +34,7 @@ namespace Humans
             Nations = Utility.LoadPresets<List<Nation>>(_nationsPath);
             _logger.LogInfo($"Number of Nation presets: {Nations.Count}");
 
+            // TODO cleaner way of doing this
             string flagPath;
             foreach (var nation in Nations)
             {
@@ -49,60 +50,6 @@ namespace Humans
                 }
             }
             _logger.LogInfo($"Number of Nations with flags: {Nations.Where(n => n.Flag != null).Count()}");
-
-            //InitializeNations();
-            //InitializeCultures();
         }
-
-        /*
-        private void InitializeNations()
-        {
-            Nations.Add(new Nation
-            {
-                Name = NationName.USA, //TODO loading of firstname and lastnames from an external files. In fact, load everything from an external json file
-                FemaleFirstNames = new List<string> { "Anna", "Ellie"},
-                MaleFirstNames = new List<string> { "Joel", "Peter" },
-                LastNames = new List<string> { "Anderson", "Smith", "Ramsey", "Pascal" },
-                FlagPath = "some path" //TODO nation flags
-            });
-
-            Nations.Add(new Nation
-            {
-                Name = NationName.Canada,
-                FemaleFirstNames = new List<string> { "FirstName1", "FirstName2", "FirstName3", "FirstName4"},
-                MaleFirstNames = new List<string> { "FirstName5", "FirstName6", "FirstName7", "FirstName8" },
-                LastNames = new List<string> { "LastName1", "LastName2", "LastName3", "LastName4" },
-                FlagPath = "some path"
-            });
-        }
-
-        private void InitializeCultures()
-        {
-            Cultures.Add(new Culture
-            {
-                Name = CultureName.American,
-                PicturePath = "some path", // TODO picture of a culture,
-                NationalityWeights = new Dictionary<NationName, int>
-                {
-                    { Nations.Find(n => n.Name == NationName.USA).Name, 10 },
-                    { Nations.Find(n => n.Name == NationName.Canada).Name, 1 }
-                },
-            });
-
-            Cultures.Add(new Culture
-            {
-                Name = CultureName.European,
-                PicturePath = "some path", // TODO picture of a culture,
-                NationalityWeights = new Dictionary<NationName, int> { } //TODO European nations
-            });
-
-            Cultures.Add(new Culture
-            {
-                Name = CultureName.Asian,
-                PicturePath = "some path", // TODO picture of a culture,
-                NationalityWeights = new Dictionary<NationName, int> { } //TODO Asians nations
-            });
-        }
-        */
     }
 }
--- src/HUMANS/UI/MainGuiController.cs
+++ src/HUMANS/UI/MainGuiController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using KSP.Game;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Humans
@@ -17,12 +18,17 @@ namespace Humans
         public Label Fullname { get; set; }
         public Label Nation { get; set; }
 
-        public Button Tab1 { get; set; }
-        public Button Tab2 { get; set; }
-        public Button Tab3 { get; set; }
+        public Button Tab1_Button { get; set; }
+        public Button Tab2_Button { get; set; }
+        public Button Tab3_Button { get; set; }
+        public VisualElement Tab1_Contents { get; set; }
+        public VisualElement Tab2_Contents { get; set; }
+        public VisualElement Tab3_Contents { get; set; }
         public Label Biography { get; set; }
 
         public Button CloseButton { get; set; }
+
+        private int _selectedKerbalId = 0;
 
         public MainGuiController()
         { }
@@ -34,56 +40,126 @@ namespace Humans
             RightBody = Root.Q<VisualElement>("right-body");
 
             Previous = Root.Q<Button>("prev");
+            Previous.RegisterCallback<PointerUpEvent>(OnPrevClicked);
             Next = Root.Q<Button>("next");
+            Next.RegisterCallback<PointerUpEvent>(OnNextClicked);
             PortraitContainer = Root.Q<VisualElement>("portrait-container");
             Portrait = Root.Q<VisualElement>("portrait");
             Flag = Root.Q<VisualElement>("flag");
             Fullname = Root.Q<Label>("fullname");
             Nation = Root.Q<Label>("nation");
 
-            Tab1 = Root.Q<Button>("tab-1");
-            Tab2 = Root.Q<Button>("tab-2");
-            Tab3 = Root.Q<Button>("tab-3");
+            Tab1_Button = Root.Q<Button>("tab-1__button");
+            Tab1_Button.RegisterCallback<PointerUpEvent>(OnTab1Clicked);
+            Tab1_Button.AddToClassList("button-tab--clicked");
+            Tab2_Button = Root.Q<Button>("tab-2__button");
+            Tab2_Button.RegisterCallback<PointerUpEvent>(OnTab2Clicked);
+            Tab2_Button.AddToClassList("button-tab--notclicked");
+            Tab3_Button = Root.Q<Button>("tab-3__button");
+            Tab3_Button.RegisterCallback<PointerUpEvent>(OnTab3Clicked);
+            Tab2_Button.AddToClassList("button-tab--notclicked");
+
+            Tab1_Contents = Root.Q<VisualElement>("tab-1__contents");
+            Tab1_Contents.style.display = DisplayStyle.Flex;
+            Tab2_Contents = Root.Q<VisualElement>("tab-2__contents");
+            Tab2_Contents.style.display = DisplayStyle.None;
+            Tab3_Contents = Root.Q<VisualElement>("tab-3__contents");
+            Tab3_Contents.style.display = DisplayStyle.None;
 
             Biography = Root.Q<Label>("biography");
 
             CloseButton = Root.Q<Button>("close-button");
             CloseButton.RegisterCallback<ClickEvent>(OnCloseButton);
 
-            Button addToPortrait = new Button { name = "addToPortrait", text = "addToPortrait" };
-            addToPortrait.RegisterCallback<ClickEvent>(evt =>
-            {
-                Portrait.style.backgroundImage = Utility.AllKerbals[0].Portrait.texture;
-            });
+            LoadHuman();
 
-            Button addToPortraitContainer = new Button { name = "addToPortraitContainer", text = "addToPortraitContainer" };
-            addToPortraitContainer.RegisterCallback<ClickEvent>(evt =>
-            {
-                PortraitContainer.style.backgroundImage = Utility.AllKerbals[1].Portrait.texture;
-            });
-
-            RightBody.Add(addToPortrait);
-            RightBody.Add(addToPortraitContainer);
-
-            LoadFirstHumanTemp();
+            Tab2_Contents.Add(new Label { text = "tab2" });
+            Tab3_Contents.Add(new Label { text = "tab3" });
         }
 
-        private void LoadFirstHumanTemp()
+        private void OnTab1Clicked(PointerUpEvent evt)
         {
-            var human = Manager.Instance.LoadedCampaign.Humans[0];
+            Tab1_Contents.style.display = DisplayStyle.Flex;
+            Tab2_Contents.style.display = DisplayStyle.None;
+            Tab3_Contents.style.display = DisplayStyle.None;
+
+            Tab1_Button.AddToClassList("button-tab--clicked");
+            Tab1_Button.RemoveFromClassList("button-tab--notclicked");
+            Tab2_Button.AddToClassList("button-tab--notclicked");
+            Tab2_Button.RemoveFromClassList("button-tab--clicked");
+            Tab3_Button.AddToClassList("button-tab--notclicked");
+            Tab3_Button.RemoveFromClassList("button-tab--clicked");
+        }
+
+        private void OnTab2Clicked(PointerUpEvent evt)
+        {
+            Tab1_Contents.style.display = DisplayStyle.None;
+            Tab2_Contents.style.display = DisplayStyle.Flex;
+            Tab3_Contents.style.display = DisplayStyle.None;
+
+            Tab1_Button.AddToClassList("button-tab--notclicked");
+            Tab1_Button.RemoveFromClassList("button-tab--clicked");
+            Tab2_Button.AddToClassList("button-tab--clicked");
+            Tab2_Button.RemoveFromClassList("button-tab--notclicked");
+            Tab3_Button.AddToClassList("button-tab--notclicked");
+            Tab3_Button.RemoveFromClassList("button-tab--clicked");
+        }
+
+        private void OnTab3Clicked(PointerUpEvent evt)
+        {
+            Tab1_Contents.style.display = DisplayStyle.None;
+            Tab2_Contents.style.display = DisplayStyle.None;
+            Tab3_Contents.style.display = DisplayStyle.Flex;
+
+            Tab1_Button.AddToClassList("button-tab--notclicked");
+            Tab1_Button.RemoveFromClassList("button-tab--clicked");
+            Tab2_Button.AddToClassList("button-tab--notclicked");
+            Tab2_Button.RemoveFromClassList("button-tab--clicked");
+            Tab3_Button.AddToClassList("button-tab--clicked");
+            Tab3_Button.RemoveFromClassList("button-tab--notclicked");
+        }
+
+        
+
+        
+
+        private void LoadHuman()
+        {
+            var kerbal = Utility.AllKerbals?.ElementAt(_selectedKerbalId) ?? null;
+            var human = Manager.Instance.LoadedCampaign.Humans.Find(h => h.Id == kerbal.Id);
+
+            if (kerbal == null) return;
 
             Fullname.text = human.FirstName + " " + human.Surname;
             Nation.text = human.Nationality;
             Portrait.style.backgroundImage = human.KerbalInfo.Portrait.texture;
 
             var nation = CulturePresets.Instance.Nations.Find(nation => nation.Name == human.Nationality);
-            
+
             if (nation != null && nation.Flag != null)
             {
                 Flag.style.backgroundImage = nation.Flag;
             }
 
-            Biography.text = human.Biography;           
+            Biography.text = human.Biography;
+        }
+
+        private void OnPrevClicked(PointerUpEvent evt)
+        {
+            if (_selectedKerbalId > 0)
+            {
+                _selectedKerbalId--;
+                LoadHuman();
+            }                
+        }
+
+        private void OnNextClicked(PointerUpEvent evt)
+        {
+            if (_selectedKerbalId < Utility.AllKerbals.Count - 1)
+            {
+                _selectedKerbalId++;
+                LoadHuman();
+            }
         }
 
         public void Update()
